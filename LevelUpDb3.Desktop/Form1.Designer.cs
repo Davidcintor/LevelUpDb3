c5bd2e903fe90c93c99da54dd829989d62365946
David Ivan� Ramirez <David_Ramirez23@unikino.edu.mx>
2025-08-09 10:56:02 -0700

    Agregamos campo folio en venta

--- LevelUpDb3.Desktop/Form1.Designer.cs
+++ LevelUpDb3.Desktop/Form1.Designer.cs
@@ -28,37 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button2 = new Button();
             SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(206, 81);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 0;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(240, 193);
-            button2.Name = "button2";
-            button2.Size = new Size(94, 29);
-            button2.TabIndex = 1;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(button2);
-            Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -66,8 +42,5 @@
         }
 
         #endregion
-
-        private Button button1;
-        private Button button2;
     }
 }
--- LevelUpDb3.Desktop/Models/Entities/Venta.cs
+++ LevelUpDb3.Desktop/Models/Entities/Venta.cs
@@ -9,6 +9,7 @@ namespace LevelUpDb3.Desktop.Models.Entities
     internal class Venta
     {
         public int Id { get; set; }
+        public int Folio { get; set; }
         public DateTime Fecha { get; set; }
         public decimal Total { get; set; }
         public int ClienteId { get; set; }
